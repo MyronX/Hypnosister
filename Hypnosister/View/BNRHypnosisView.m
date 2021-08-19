66ce7e26d1a65153399c9bf95b95f075f3e46aeb
Myron <513482380@qq.com>
2021-08-19 23:41:13 +0800

    draw circular

--- Hypnosister/View/BNRHypnosisView.m
+++ Hypnosister/View/BNRHypnosisView.m
@@ -17,4 +17,37 @@
 }
 */
 
+- (void)drawRect:(CGRect)rect {
+    CGRect bounds = self.bounds;
+    
+    //根据bounds计算中心
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2;
+    center.y = bounds.origin.y + bounds.size.height / 2;
+    
+    //计算圆形的半径
+    float maxRadius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    //画原
+    //[path addArcWithCenter:center radius:radius startAngle:0.0 endAngle:M_PI * 2 clockwise:YES];
+    
+    //绘制同心圆
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        //抬笔
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        
+        [path addArcWithCenter:center radius:currentRadius startAngle:0.0 endAngle:M_PI * 2 clockwise:YES];
+    }
+    
+    
+    
+    //添加各种属性
+    path.lineWidth = 10;
+    [[UIColor lightGrayColor] setStroke];
+    
+    [path stroke];
+}
+
 @end
--- Hypnosister/ViewController.m
+++ Hypnosister/ViewController.m
@@ -19,10 +19,19 @@
     // Do any additional setup after loading the view.
     
     
-    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    //CGRect firstFrame = CGRectMake(80, 240, 200, 300);
+    
+    CGRect firstFrame = self.view.bounds;
     BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
-    firstView.backgroundColor = [UIColor redColor];
+    firstView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:firstView];
+    
+    
+//    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
+//    BNRHypnosisView *secondView = [[BNRHypnosisView alloc] initWithFrame:secondFrame];
+//    secondView.backgroundColor = [UIColor blueColor];
+//    //[self.view addSubview:secondView];
+//    [firstView addSubview:secondView];
     
     
 }
